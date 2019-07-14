2bc37d8703f6aa00aaa9030b6183165fe573f80d
maygalibter <may.galibter@icloud.com>
2019-07-14 14:18:29 +0300

    Add files via upload

---
+++ FactoryMethod/Segway.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Segway : Vehicle
+    {
+        protected string driverCity;
+
+        public Segway(string model, float cylinder, string color, int numberOfWheels, string driverCity) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.driverCity = driverCity;
+        }
+
+        public override string ToString()
+        {
+            return "Segway " + base.ToString() + $" {driverCity}";
+        }
+    }
+}
---
+++ FactoryMethod/Truck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Truck : Vehicle
+    {
+        protected string driverTatto;
+
+        public Truck(string model, float cylinder, string color, int numberOfWheels, string driverTatto) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.driverTatto = driverTatto;
+        }
+
+        public override string ToString()
+        {
+            return "Truck " + base.ToString() + $" {driverTatto}";
+        }
+    }
+}
