b2b5f71d9003c915725a1dade44e95cd466ae9b0
maygalibter <may.galibter@icloud.com>
2019-07-17 14:52:53 +0300

    Add files via upload

---
+++ Builder/BurgerFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+        public class BurgerFactory
+        {
+            public Hamburger CreateBurger(string burgerType)
+            {
+                if (burgerType == "veggie" )
+                {
+                    Console.WriteLine("Creating veggie burger...");
+                    VeggHamburgerBuilder b = new VeggHamburgerBuilder();
+                    b.BuildHamburger();
+
+                    return b.GetHamburger();
+
+                }
+                else if (burgerType == "veggie lettuce")
+                {
+                    Console.WriteLine("Creating veggie lettuce burger...");
+                    VeggHambBuilderLettuce b = new VeggHambBuilderLettuce();
+                    b.BuildHamburger();
+
+                    return b.GetHamburger();
+
+                }
+                else if (burgerType == "susu")
+                {
+                    Console.WriteLine("Creating susu burger...");
+                    SusuBuilder b = new SusuBuilder();
+                    b.BuildHamburger();
+
+                    return b.GetHamburger();
+
+                }
+                else if (burgerType == "susu lettuce")
+                {
+                    Console.WriteLine("Creating susu lettuce burger...");
+                    SusuBuilderLettuce b = new SusuBuilderLettuce();
+                    b.BuildHamburger();
+
+                    return b.GetHamburger();
+                }
+
+            throw new ArgumentException($"cannot create {burgerType} burger!");
+
+             }
+        }
+    
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<HamburgerBuilder> menu = new List<HamburgerBuilder>();
+            menu.Add(new VeggHamburgerBuilder());
+            menu.Add(new VeggHambBuilderLettuce());
+            menu.Add(new SusuBuilder());
+            menu.Add(new SusuBuilderLettuce());
+
+            Hamburger hamb = PrepareHamburger(menu[0]);
+
+            // Burger Factory
+            BurgerFactory bFactory = new BurgerFactory();
+
+            Hamburger v = bFactory.CreateBurger("veggie");
+            Hamburger vl = bFactory.CreateBurger("veggie lettuce");
+            Hamburger s = bFactory.CreateBurger("susu");
+            Hamburger sl = bFactory.CreateBurger("susu lettuce");
+        }
+
+        static Hamburger PrepareHamburger(HamburgerBuilder builder)
+        {
+            builder.BuildHamburger();
+            return builder.GetHamburger();
+        }
+    }
+}
